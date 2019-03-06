45776c3d9529bd028ea2161fe512ecb0117fdf3a
siegrest <179900@ttu.ee>
2019-03-06 19:40:31 +0200

    Replace RPC library with source

---
+++ DiscordRPC/Registry/UriScheme.cs
@@ -0,0 +1,48 @@
+﻿using DiscordRPC.Logging;
+using System;
+using System.Diagnostics;
+
+namespace DiscordRPC.Registry
+{
+    internal static class UriScheme
+	{
+        public static void RegisterUriScheme(ILogger logger, string appid, string steamid = null)
+        {
+            //Get the creator
+            IUriSchemeCreator creator = null;
+            switch(Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32Windows:
+                case PlatformID.Win32S:
+                case PlatformID.Win32NT:
+                case PlatformID.WinCE:
+                    creator = new WindowsUriSchemeCreator();
+                    break;
+
+                case PlatformID.Unix:
+                    creator = new UnixUriSchemeCreator();
+                    break;
+                
+                //case PlatformID.MacOSX:
+                //    creator = new MacUriSchemeCreator();
+                //    break;
+            }
+
+            //Make sure its valid
+            if (creator == null)
+                throw new PlatformNotSupportedException("Platform does not support registration.");
+
+            //Register the endpoitns
+            creator.RegisterUriScheme(logger, appid, steamid);
+        }
+
+        /// <summary>
+        /// Gets the current location of the app
+        /// </summary>
+        /// <returns></returns>
+        public static string GetApplicationLocation()
+        {
+            return Process.GetCurrentProcess().MainModule.FileName;
+        }
+    }
+}
--- Service/Settings.cs
+++ Service/Settings.cs
@@ -7,7 +7,7 @@ namespace Service {
         [JsonIgnore] public const string ProgramName = "Exiled Presence";
         [JsonIgnore] public const string GameWindowTitle = "Path of Exile";
         [JsonIgnore] public const string ConfigFileName = "config.json";
-        [JsonIgnore] public const string Version = "v1.0";
+        [JsonIgnore] public const string Version = "v1.0.1";
         [JsonIgnore] public const int CharacterUpdateDelaySec = 60;
         [JsonIgnore] public const int PresencePollDelayMs = 500;
         [JsonIgnore] private const int UpdateCheckIntervalH = 24;
